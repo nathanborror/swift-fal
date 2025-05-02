0bb4c8f0c1b9069e4c31f7054151e497b6ebb3a2
Nathan Borror <nathan@nathanborror.com>
2025-05-02 10:03:12 -0700

    dead code

--- Sources/Fal/Defaults.swift
+++ Sources/Fal/Defaults.swift
@@ -2,8 +2,6 @@ import Foundation
 
 public struct Defaults {
     
-    public static let apiHost = URL(string: "https://fal.run/fal-ai")!
-    
     public static let models: [Model] = [
         .init(id: "fast-sdxl", name: "Stable Diffusion XL", owner: "fal"),
         .init(id: "stable-cascade", name: "Stable Cascade", owner: "fal"),
